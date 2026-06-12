4eebba193b2b2ac6f9f191ef6864f9d88798991d
miguel <74296724@continental.edu.pe>
2026-06-12 12:53:21 -0500

    nueva

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-print("tercer intento github")
-print("ya casi termina el semestre")
+n = int(input("ingrese el primer numero:"))
+n2 = int(input("ingrese el segundo numero: "))
+
+suma= n + n2
+print("la suma es: "{suma})
